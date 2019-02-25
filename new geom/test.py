3ba9b3f0891c29b062c6bd038c8b6424539c6d95
Andrew Reilley <reilleya@mit.edu>
2019-02-25 00:27:43 -0500

    Added port area calculation and better geometry generation

--- new geom/test.py
+++ new geom/test.py
@@ -9,23 +9,75 @@ from skimage import measure
 X, Y = np.meshgrid(np.linspace(-1,1,1001), np.linspace(-1,1,1001))
 phi = np.ones_like(X)
 
+# Bates
+"""
+r = 0.39
+phi[X**2 + Y**2 < r**2] = 0
+"""
+
 # Moon burner
-#phi[(X-0.5)**2 + Y**2 < 0.125] = 0
+"""
+offset = 0.55
+r = 0.28
+phi[(X-offset)**2 + Y**2 < r**2] = 0
+"""
 
-# Core
-phi[X**2 + Y**2 < 0.175] = 0
 
-# Fins
-phi[np.logical_and(np.abs(Y)<0.125, np.abs(X)<0.7)] = 0
-phi[np.logical_and(np.abs(X)<0.125, np.abs(Y)<0.7)] = 0
-phi[np.logical_and(np.logical_and(X+Y<0.176, X+Y>-0.176), np.logical_and(X - 1 < Y, X + 1 > Y))] = 0
-phi[np.logical_and(np.logical_and(X-Y<0.176, X-Y>-0.176), np.logical_and(-X - 1 < Y, -X + 1 > Y))] = 0
+# X core
+"""
+w = 0.125
+l = 0.7
+phi[np.logical_and(np.abs(Y)<w/2, np.abs(X)<l)] = 0
+phi[np.logical_and(np.abs(X)<w/2, np.abs(Y)<l)] = 0
+"""
+
+# C slot
+"""
+w = 0.125
+offset = 0.5
+phi[np.logical_and(np.abs(Y)<w/2, X>offset)] = 0
+"""
+
+# D grain
+"""
+offset = 0.5
+phi[X>offset] = 0
+"""
+
+# Finocyl
+"""
+w = 0.1366
+l = 0.272
+n = 8
+r = 0.39
+
+phi[X**2 + Y**2 < r**2] = 0
+l += r
+for i in range(0, n):
+    th = 2*np.pi/n * i
+    a = np.cos(th)
+    b = np.sin(th)
+    vect = abs(a*X + b*Y) < w/2
+    near = b*X - a*Y > 0
+    far = b*X - a*Y < l
+    ends = np.logical_and(far, near)
+    phi[np.logical_and(vect, ends)] = 0
+"""
+
+# Face
+
+phi[(2*X-0.65)**2 + (Y+0.35)**2 < 0.05] = 0
+phi[(2*X+0.65)**2 + (Y+0.35)**2 < 0.05] = 0
+phi[(X/2.5)**2 + (2*Y-0.7)**2 < 0.07] = 0
+phi[(X/2.5)**2 + (2*Y-0.5)**2 < 0.07] = 1
+
 
 # Setup grain boundry (casting tube)
 mask = X**2 + Y**2 > 1
 
 phi  = np.ma.MaskedArray(phi, mask)
 t    = skfmm.distance(phi, dx=1e-3)
+#plt.imshow(phi)
 plt.contour(X, Y, phi, [0], linewidths=(2), colors='black')
 plt.contour(X, Y, phi.mask, [0], linewidths=(2), colors='red')
 plt.contourf(X, Y, t, 20)
@@ -42,12 +94,16 @@ def clean(contour, m = 498):
     return contour[l < m]
 
 p = [0]
+a = [np.count_nonzero(t == 0)]
 
 fig, ax = plt.subplots()
 
-for i in range(0, 150, 3):
+valid = np.logical_not(mask)
+
+for i in range(0, 300, 1):
     contours = measure.find_contours(t, i/500, fully_connected='high')
     p.append(0)
+    a.append(np.count_nonzero(np.logical_and(t <= (i/500), valid)))
     for n, contour in enumerate(contours):
         contour = clean(contour)
         p[-1] += length(contour)
@@ -56,4 +112,8 @@ for i in range(0, 150, 3):
 ax.imshow(t, interpolation='nearest', cmap=plt.cm.gray)
 plt.show()
 plt.plot(p)
+plt.show()
+plt.plot(a)
+plt.show()
+plt.imshow(t)
 plt.show()
