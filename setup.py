a3782357c8fb639534bd6a3a5b111dbda863dc97
Aidan Sojourner <aidan.sojourner@gmail.com>
2019-04-28 16:17:04 -0700

    Update README and setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 from uilib.fileIO import appVersionStr
-# import build_ui
+
 try:
     from pyqt_distutils.build_ui import build_ui
     cmdclass = {'build_ui': build_ui}
@@ -8,14 +8,13 @@ except ImportError:
     build_ui = None  # user won't have pyqt_distutils when deploying
     cmdclass = {}
 
-requirements = list(open('requirements.txt').read().splitlines())
-
 setup(
     name='openMotor',
     version=appVersionStr,
+    license='GPLv3',
     packages=find_packages(),
     url='https://github.com/reilleya/openMotor',
-    install_requires=requirements,
+    description='An open-source internal ballistics simulator for rocket motor experimenters',
     long_description=open('README.md').read(),
     cmdclass=cmdclass
 )
