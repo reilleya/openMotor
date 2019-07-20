dff7e95607a68d5d2aaae3826aa2a95a213870b4
Joe Quigley <quigley.joseph@outlook.com>
2019-07-20 17:16:27 -0400

    Change: Set EnableHighDpiScaling flag for high DPI support
    This is handled at the OS layer by OSX and at the server layer by Wayland, whereas X11 and Windows place the responsibility of scaling on the app. Qt includes a flag to enable high-dpi support trivially, though it causes the app to be unwieldly large.
    
    Fixes #101

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import sys
-import app
+from app import App
+from PyQt5.QtCore import Qt
 
-app = app.App(sys.argv)
+# must be set before the app is constructed
+# cf. https://doc.qt.io/qt-5/highdpi.html
+# and https://www.riverbankcomputing.com/static/Docs/PyQt5/api/qtcore/qt.html#ApplicationAttribute
+App.setAttribute(Qt.AA_EnableHighDpiScaling)
+
+app = App(sys.argv)
 sys.exit(app.exec())
