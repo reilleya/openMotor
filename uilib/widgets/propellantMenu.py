06473a3d91543317b8fa1d79a2b14e892127f841
Austin <32467683+aushe02@users.noreply.github.com>
2021-05-10 23:02:56 -0400

    Delete grains and propellants with backspace key (#190)
    * Added delete grain with backspace or delete key
    * Added delete propellant with backspace or delete key

--- uilib/widgets/propellantMenu.py
+++ uilib/widgets/propellantMenu.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QDialog, QMessageBox, QApplication
 from PyQt5.QtCore import pyqtSignal
 from ..helpers import FLAGS_NO_ICON
+from PyQt5.QtCore import Qt
 
 import motorlib.propellant
 
@@ -132,3 +133,8 @@ class PropellantMenu(QDialog):
             self.propEdited(self.ui.propEditor.getProperties())
             return True
         return res == QMessageBox.Discard
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Delete or event.key() == Qt.Key_Backspace:
+            if len(self.ui.listWidgetPropellants.selectedItems()) != 0:
+                self.deleteProp()
