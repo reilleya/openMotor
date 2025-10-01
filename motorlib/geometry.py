bbbc804409a2385d7082f9be6968cf111e92a016
revo1v3r <e11415527@gmail.com>
2025-10-01 15:46:56 +0300

    Geometry: update type hints and tests

--- motorlib/geometry.py
+++ motorlib/geometry.py
@@ -1,53 +1,80 @@
 """This module includes the geometry methods that openMotor uses in its calculations"""
 
 import math
-import numpy as np
+from typing import Union
 
-def circleArea(dia):
+import numpy as np
+from numpy.typing import NDArray
+
+
+def circleArea(dia: float) -> float:
     """Returns the area of a circle with diameter dia"""
     return ((dia / 2) ** 2) * math.pi
 
-def circlePerimeter(dia):
+
+def circlePerimeter(dia: float) -> float:
     """Returns the perimeter (circumference) of a circle with diameter dia"""
     return dia * math.pi
 
-def circleDiameterFromArea(area):
+
+def circleDiameterFromArea(area: float) -> float:
     """Returns the diameter of a circle with area 'area'"""
     return 2 * ((area / math.pi) ** 0.5)
 
-def tubeArea(dia, height):
+
+def tubeArea(dia: float, height: int) -> float:
     """Returns the surface area of a tube (cylinder without endcaps) with diameter 'dia' and height 'height'"""
     return dia * math.pi * height
 
-def cylinderArea(dia, height):
+
+def cylinderArea(dia: float, height: int) -> float:
     """Returns the surface area of a cylinder with diameter 'dia' and height 'height'"""
     return (2 * circleArea(dia)) + (tubeArea(dia, height))
 
-def cylinderVolume(dia, height):
+
+def cylinderVolume(dia: float, height: int) -> float:
     """Returns the volume of a cylinder with diameter 'dia' and height 'height'"""
     return height * circleArea(dia)
 
-def frustumLateralSurfaceArea(diameterA, diameterB, length):
+
+def frustumLateralSurfaceArea(diameterA: int, diameterB: int, length: int) -> float:
     """Returns the surface area of a frustum (truncated cone) with end diameters A and B and length 'length'"""
     radiusA = diameterA / 2
     radiusB = diameterB / 2
-    return math.pi * (radiusA + radiusB) * (abs(radiusA - radiusB) ** 2 + length ** 2) ** 0.5
+    return (
+        math.pi * (radiusA + radiusB) * (abs(radiusA - radiusB) ** 2 + length**2) ** 0.5
+    )
 
-def frustumVolume(diameterA, diameterB, length):
+
+def frustumVolume(diameterA: int, diameterB: int, length: int) -> float:
     """Returns the volume of a frustum (truncated cone) with end diameters A and B and length 'length'"""
     radiusA = diameterA / 2
     radiusB = diameterB / 2
-    return math.pi * (length / 3) * (radiusA ** 2 + radiusA * radiusB + radiusB ** 2)
+    return math.pi * (length / 3) * (radiusA**2 + radiusA * radiusB + radiusB**2)
 
-def splitFrustum(diameterA, diameterB, length, splitPosition):
+
+def splitFrustum(
+    diameterA: int, diameterB: int, length: int, splitPosition: int
+) -> tuple[tuple[int, float, int], tuple[float, int, int]]:
     """Takes in info about a frustum (truncated cone) and a position measured from the "diameterA" and returns
     a tuple of frustums representing the two halves of the original frustum if it were split on the plane at
     distance "position" from the face with diameter "diameterA"
     """
-    splitDiameter = diameterA + (diameterB - diameterA) * (splitPosition / length)
-    return (diameterA, splitDiameter, splitPosition), (splitDiameter, diameterB, length - splitPosition)
+    splitDiameter: float = diameterA + (diameterB - diameterA) * (
+        splitPosition / length
+    )
+    return (diameterA, splitDiameter, splitPosition), (
+        splitDiameter,
+        diameterB,
+        length - splitPosition,
+    )
 
-def length(contour, mapSize, tolerance=3):
+
+def length(
+    contour: NDArray[Union[np.int_, np.float64]],
+    mapSize: Union[int, float],
+    tolerance: int = 3,
+) -> Union[int, float]:
     """Returns the total length of all segments in a contour that aren't within 'tolerance' of the edge of a
     circle with diameter 'mapSize'"""
     offset = np.roll(contour.T, 1, axis=1)
@@ -60,13 +87,19 @@ def length(contour, mapSize, tolerance=3):
 
     return np.sum(lengths[valid])
 
-def clean(contour, mapSize, tolerance):
+
+def clean(
+    contour: NDArray[Union[np.int_, np.float64]],
+    mapSize: Union[int, float],
+    tolerance: int,
+) -> NDArray[Union[np.int_, np.float64]]:
     """Returns a contour with the same points as the input, omitting any within 'tolerace' of a circle of
     diameter 'mapSize'"""
     offset = np.array([[mapSize / 2, mapSize / 2]])
     lengths = np.linalg.norm(contour - offset, axis=1)
     return contour[lengths < (mapSize / 2) - tolerance]
 
-def dist(point1, point2):
+
+def dist(point1: tuple[int, int], point2: tuple[int, int]) -> int:
     """Returns the distance between two points [x1, y1], [x2, y2]"""
     return ((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2) ** 0.5
--- test/unit/geometry.py
+++ test/unit/geometry.py
@@ -1,48 +1,72 @@
 import unittest
+
 import motorlib.geometry
 
+
 class TestGeometryMethods(unittest.TestCase):
-    def test_circleArea(self):
+    def test_circleArea(self) -> None:
         self.assertAlmostEqual(motorlib.geometry.circleArea(0.5), 0.19634954)
 
-    def test_circlePerimeter(self):
+    def test_circlePerimeter(self) -> None:
         self.assertAlmostEqual(motorlib.geometry.circlePerimeter(0.5), 1.57079633)
 
-    def test_circleDiameterFromArea(self):
-        self.assertAlmostEqual(motorlib.geometry.circleDiameterFromArea(0.19634954), 0.5)
+    def test_circleDiameterFromArea(self) -> None:
+        self.assertAlmostEqual(
+            motorlib.geometry.circleDiameterFromArea(0.19634954), 0.5
+        )
 
-    def test_tubeArea(self):
+    def test_tubeArea(self) -> None:
         self.assertAlmostEqual(motorlib.geometry.tubeArea(0.5, 2), 3.14159265)
 
-    def test_cylinderArea(self):
+    def test_cylinderArea(self) -> None:
         self.assertAlmostEqual(motorlib.geometry.cylinderArea(0.5, 2), 3.53429174)
 
-    def test_cylinderVolume(self):
+    def test_cylinderVolume(self) -> None:
         self.assertAlmostEqual(motorlib.geometry.cylinderVolume(0.5, 2), 0.39269908)
 
-    def test_frustumLateralSurfaceArea(self):
-        self.assertAlmostEqual(motorlib.geometry.frustumLateralSurfaceArea(2, 3, 5), 39.46576927)
+    def test_frustumLateralSurfaceArea(self) -> None:
+        self.assertAlmostEqual(
+            motorlib.geometry.frustumLateralSurfaceArea(2, 3, 5), 39.46576927
+        )
 
-    def test_frustumVolume(self):
-        # Cone case
+    def test_frustumVolumeConeCase(self) -> None:
         self.assertAlmostEqual(motorlib.geometry.frustumVolume(0, 10, 10), 261.79938779)
-        # Frustum case
-        self.assertAlmostEqual(motorlib.geometry.frustumVolume(10, 30, 50), 17016.96020694)
 
-    def test_splitFrustum(self):
-        # Simple case
-        self.assertAlmostEqual(motorlib.geometry.splitFrustum(1, 2, 4, 2), ((1, 1.5, 2), (1.5, 2, 2)))
-        # Inverted case
-        self.assertAlmostEqual(motorlib.geometry.splitFrustum(2, 1, 4, 2), ((2, 1.5, 2), (1.5, 1, 2)))
-        # Make sure that the connected ends of the frustums line up
+    def test_frustumVolumeFrustumCase(self) -> None:
+        self.assertAlmostEqual(
+            motorlib.geometry.frustumVolume(10, 30, 50), 17016.96020694
+        )
+
+    def test_splitFrustumSimpleCase(self) -> None:
+        self.assertEqual(
+            motorlib.geometry.splitFrustum(1, 2, 4, 2), ((1, 1.5, 2), (1.5, 2, 2))
+        )
+
+    def test_splitFrustumInvertedCase(self) -> None:
+        self.assertEqual(
+            motorlib.geometry.splitFrustum(2, 1, 4, 2), ((2, 1.5, 2), (1.5, 1, 2))
+        )
+
+    def test_splitFrustum(self) -> None:
+        """Make sure that the connected ends of the frustums line up"""
         upper, lower = motorlib.geometry.splitFrustum(1, 3, 3, 1)
         self.assertEqual(upper[1], lower[0])
 
-    def test_dist(self):
+    def test_dist_same_point(self) -> None:
         self.assertEqual(motorlib.geometry.dist((5, 5), (5, 5)), 0)
-        self.assertEqual(motorlib.geometry.dist((5, 5), (6, 5)), 1)
-        self.assertEqual(motorlib.geometry.dist((5, 5), (5, 6)), 1)
-        self.assertEqual(motorlib.geometry.dist((0, 0), (-1, -1)), 2 ** 0.5)
 
-if __name__ == '__main__':
+    def test_dist_horizontal_one_unit(self) -> None:
+        self.assertEqual(motorlib.geometry.dist((5, 5), (6, 5)), 1)
+
+    def test_dist_vertical_one_unit(self) -> None:
+        self.assertEqual(motorlib.geometry.dist((5, 5), (5, 6)), 1)
+
+    def test_dist_diagonal_negative_coords(self) -> None:
+        expected_distance = 2**0.5  # sqrt(2)
+        self.assertAlmostEqual(
+            motorlib.geometry.dist((0, 0), (-1, -1)), expected_distance
+        )
+
+
+if __name__ == "__main__":
     unittest.main()
