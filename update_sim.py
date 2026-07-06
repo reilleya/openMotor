ee831645b0472f8149ee878e431bbbb66c0c9481
aydoganuzun093 <aydoganuzun093@gmail.com>
2026-07-06 08:39:01 +0300

    Delete update_sim.py

--- update_sim.py
+++
@@ -1,7 +0,0 @@
-import re
-
-with open('sim.py', 'r', encoding='utf-8') as f:
-    code = f.read()
-
-# Replace the simulation logic with steady state
-# We will do this via a few modifications directly on sim.py
